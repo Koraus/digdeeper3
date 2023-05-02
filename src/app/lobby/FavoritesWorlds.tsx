223d6c5d75f19ce39170e13b102365b611d55f72
Koraus <dmytro.kashchynets@gmail.com>
2023-05-02 10:20:13 +0200

    lobby update

--- src/app/lobby/FavoritesWorlds.tsx
+++ src/app/lobby/FavoritesWorlds.tsx
@@ -9,14 +9,24 @@ export function FavoritesWorlds({
 
     const [isFavorites, setIsFavorites] = useState(false);
 
-    return <div {...props}><h3
-        onClick={() => setIsFavorites(!isFavorites)}
-        css={[{
-            margin: "0.9vmin 0",
-        }]}>  <Bookmarks css={[{
-            width: "2vmin",
-            marginRight: "0.4vmin",
-        }]} />favorites:</h3>
+    return <div {...props}>
+
+        <h3
+            onClick={() => setIsFavorites(!isFavorites)}
+            css={[{
+                margin: "0.9vmin 0",
+            }]}>
+            <div css={[{
+                margin: "0.9vmin 0",
+                display: "inline-block"
+
+            }]}>
+                &#9658;
+            </div>
+             favorites: <Bookmarks css={[{
+                width: "2vmin",
+                marginRight: "0.4vmin",
+            }]} /></h3>
         {isFavorites &&
             <div> <span>111</span> </div>
         }
