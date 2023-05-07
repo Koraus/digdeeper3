784147785e7e3620cf47d1afacfd9e949a8d432c
Andriy Kashchynets <andriy.kashchynets@gmail.com>
2023-05-07 10:56:44 +0300

    smooth camera

--- src/app/mainSscene/MainScene.tsx
+++ src/app/mainSscene/MainScene.tsx
@@ -3,9 +3,11 @@ import { CellView } from "./CellView";
 import { PlayerView } from "./PlayerView";
 import { CopilotView } from "./CopilotView";
 import { StopLine } from "./StopLine";
+import { GroupSync } from "../../utils/GroupSync";
+import { Vector3 } from "three";
+import { dampVector3 } from "../../utils/dampVector3";
 
 export function MainScene() {
-
     return <>
         <color attach="background" args={["#0a000d"]} />
 
@@ -23,21 +25,41 @@ export function MainScene() {
             <GizmoViewport />
         </GizmoHelper>
 
+        <PerspectiveCamera
+            makeDefault
+            fov={60}
+            near={0.1}
+            far={1000}
+        >
+        </PerspectiveCamera>
+
         <PlayerView>
-            <PerspectiveCamera
-                makeDefault
-                fov={60}
-                near={0.1}
-                far={1000}
-                position={[0, 6, 30]}
-                rotation={[-0.3, 0, 0]}
-            >
-            </PerspectiveCamera>
+            <GroupSync
+                onFrame={(g, { camera }, delta) => {
+                    const z = new Vector3(0, 0, 0);
+                    g.localToWorld(z);
+                    camera.parent?.worldToLocal(z);
+
+                    const p = new Vector3(0, 6, 30);
+                    g.localToWorld(p);
+                    camera.parent?.worldToLocal(p);
+
+                    if (camera.position.distanceTo(p) > 10) {
+                        camera.position.copy(p);
+                    } else if (camera.position.distanceTo(p) > 0.1) {
+                        dampVector3(
+                            camera.position, camera.position, p, 0.1, delta);
+                    } else {
+                        // 
+                    }
+
+                    camera.lookAt(z);
+                }} />
         </PlayerView>
         <CopilotView />
 
         <StopLine />
-        
+
         {(function* () {
             const tCellsPerScreen = 61;
             const xCellsPerScreen = 51;
