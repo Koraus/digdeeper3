77b9dc190bb566c2be30d4b666acb7747a447c4b
Koraus <dmytro.kashchynets@gmail.com>
2023-05-11 16:55:32 +0200

    Added information about the current gameplay

--- src/app/lobby/СurrentWorldInfo.tsx
+++ src/app/lobby/СurrentWorldInfo.tsx
@@ -7,17 +7,28 @@ import { trekRecoil } from "../trekRecoil";
 export function СurrentWorldInfo() {
     const progression = useRecoilValue(trekRecoil);
     const dropzone = trekDropzone(progression);
-
+    const pStyle = { margin: "2px 10px" };
+    
     return <div css={[{ display: "flex" }]}>
-        <DropzonePreview  css={[{ 
-            margin: "10px" }]} 
-         dropzone={dropzone} />
-        <div>
-            <p>seed: {dropzone.seed}</p>
-            <p>rule: {dropzone.world.ca.rule}</p>
-            <p>stateCount: {dropzone.world.ca.stateCount}</p>
-            <p>version: {dropzone.world.ca.version}</p>
+        <DropzonePreview css={[{
+            margin: "10px",
+        }]}
+            dropzone={dropzone} />
+        <div  css={[{marginTop: "10px"}]} >
+            <p css={[pStyle]}> sightVersion: {dropzone.world.sightVersion} </p>
+            <p css={[pStyle]}>ca version: {dropzone.world.ca.version}</p>
+            <p css={[pStyle]}>ca stateCount: {dropzone.world.ca.stateCount}</p>
+            <p css={[pStyle]}>ca rule: {dropzone.world.ca.rule} </p>
+            <p css={[pStyle]}>stateEnergyDrain:
+                {dropzone.world.stateEnergyDrain} </p>
+            <p css={[pStyle]}>stateEnergyGain:
+                {dropzone.world.stateEnergyGain}</p>
+            <p css={[pStyle]}>emptyState: {dropzone.world.emptyState}</p>
+            <p css={[pStyle]}>seed: {dropzone.seed}</p>
+            <p css={[pStyle]}>width: {dropzone.width}</p>
+            <p css={[pStyle]}>depthLeftBehind: {dropzone.depthLeftBehind}</p>
         </div>
+
 
     </div>;
 }
