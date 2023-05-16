a193190d63b243e8a66eedb373c165ea8c85e487
Koraus <dmytro.kashchynets@gmail.com>
2023-05-16 11:28:29 +0200

    the display of stateEnergyDrain and stateEnergyGain is fixed

--- src/app/lobby/СurrentWorldInfo.tsx
+++ src/app/lobby/СurrentWorldInfo.tsx
@@ -14,15 +14,21 @@ export function СurrentWorldInfo() {
             margin: "10px",
         }]}
             dropzone={dropzone} />
-        <div  css={[{marginTop: "10px"}]} >
+        <div css={[{ marginTop: "10px" }]} >
             <p css={[pStyle]}> sightVersion: {dropzone.world.sightVersion} </p>
             <p css={[pStyle]}>ca version: {dropzone.world.ca.version}</p>
             <p css={[pStyle]}>ca stateCount: {dropzone.world.ca.stateCount}</p>
             <p css={[pStyle]}>ca rule: {dropzone.world.ca.rule} </p>
-            <p css={[pStyle]}>stateEnergyDrain:
-                {dropzone.world.stateEnergyDrain} </p>
-            <p css={[pStyle]}>stateEnergyGain:
-                {dropzone.world.stateEnergyGain}</p>
+            <p css={[pStyle]}>stateEnergyDrain:&#32;
+                {dropzone.world.stateEnergyDrain[0]}&nbsp;
+                {dropzone.world.stateEnergyDrain[1]}&nbsp;
+                {dropzone.world.stateEnergyDrain[2]}&nbsp;
+            </p>
+            <p css={[pStyle]}>stateEnergyGain:&#32;
+                {dropzone.world.stateEnergyGain[0]}&nbsp;
+                {dropzone.world.stateEnergyGain[1]}&nbsp;
+                {dropzone.world.stateEnergyGain[2]}&nbsp;
+            </p>
             <p css={[pStyle]}>emptyState: {dropzone.world.emptyState}</p>
             <p css={[pStyle]}>seed: {dropzone.seed}</p>
             <p css={[pStyle]}>width: {dropzone.width}</p>
