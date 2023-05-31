d787deef8417e09915e015a9dc5b8c72c20506f7
Koraus <dmytro.kashchynets@gmail.com>
2023-05-31 17:38:25 +0200

    Add color themes

--- src/app/mainSscene/cells/Cell.tsx
+++ src/app/mainSscene/cells/Cell.tsx
@@ -18,12 +18,62 @@ export const floorColors = ({
     energy: new Color("#ffc57a"),
 } as const);
 
+const colorThemes = [{
+    // earth-like vegetation of mideteranian climate
+    rock: {
+        mainColor: new Color("#2e444f"),
+        snowColor: undefined,
+    },
+    grass: new Color("#00ff00"),
+    pickable: new Color("#b635d3"),
+    floor: {
+        rock: new Color("#576c6e"),
+        grass: new Color("#d8dd76"),
+        energy: new Color("#ffc57a"),
+    },
+    bricks: new Color("#fb9658"),
+}, {
+    // [name]
+    rock: {
+        mainColor: new Color("#782881"),
+        snowColor: undefined,
+    },
+    grass: new Color("#980843"),
+    pickable: new Color("#61C3EA"),
+    floor: {
+        rock: new Color("#2d282e"),
+        grass: new Color("#6a3e4f"),
+        energy: new Color("#a947b4"),
+    },
+    bricks: new Color("#bf4967"),
+}, {
+    // [name]
+    rock: {
+        mainColor: new Color("#0b76ab"),
+        snowColor: new Color("#cae0fb"),
+    },
+    rockSnow: new Color("#cae0fb"),
+    grass: new Color("#e66ce8"),
+    pickable: new Color("#5bef20"),
+    floor: {
+        rock: new Color("#576c6e"),
+        grass: new Color("#d8dd76"),
+        energy: new Color("#ffc57a"),
+    },
+    bricks: new Color("#fb9658"),
+}] as const;
+
 export function Cell(ctx: LayoutContext) {
+
     const {
         rootMatrixWorld,
         state: { t, x, isVisited, dropzone, isCollected, trek },
         abuseBox,
     } = ctx;
+
+    // const colors = colorThemes[Math.floor(t / 15) % colorThemes.length];
+    const colors = colorThemes[
+        Number(dropzone.world.ca.rule[0]) % colorThemes.length];
 
     const expSight = epxandedSight(trek);
     const caState = caForDropzone(dropzone)._at(t, x);
@@ -35,32 +85,31 @@ export function Cell(ctx: LayoutContext) {
     const visualKey = isGrass ? "grass" : isRock ? "rock" : "energy";
 
     const floor = abuseBox();
-    floor.setColor(floorColors[visualKey]);
+    floor.setColor(colors.floor[visualKey]);
     floor.setMatrix(_m4s[1].compose(
         _v3s[0].set(0, -1, 0),
         _qs[0].identity(),
         _v3s[1].set(1, 2, 1),
     ).premultiply(rootMatrixWorld));
-
     if (isEnergy) {
         if (isCollected) {
             const isJustCollected = isCollected && !ctx.prevState?.isCollected;
             if (isJustCollected) {
-                PickablePick(ctx);
+                PickablePick(colors.pickable, ctx);
             }
         } else {
-            Pickable(ctx);
+            Pickable(colors.pickable, ctx);
         }
     }
 
     if (!isVisited) {
         if (isGrass) {
-            Grass(ctx);
+            Grass(colors.grass, ctx);
         }
         if (isRock) {
-            Rock(ctx);
+            Rock(colors.rock, ctx);
         }
     } else {
-        Bricks(ctx);
+        Bricks(colors.bricks, ctx);
     }
 }
