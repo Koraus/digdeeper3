32a5713e69ab27931b301dc7c801bb6a11253344
Andriy Kashchynets <andriy.kashchynets@gmail.com>
2023-06-04 12:42:36 +0300

    tweaked filter

--- src/app/basecamp/NewDropzones.tsx
+++ src/app/basecamp/NewDropzones.tsx
@@ -17,26 +17,46 @@ const generators = {
 export function NewDropzones({
     ...props
 }: jsx.JSX.IntrinsicElements["div"]) {
-    const [dropzones, setDropzones] = useState<Dropzone[]>();
+    const [dropzones, setDropzones] = useState<Dropzone[]>([]);
 
     const [generator, setGenerator] =
         useState<keyof typeof generators>("symmetrical");
 
     const setDropzone = useSetDropzone();
 
-    const [isFiltered, setIsFiltered] = useState(false);
+    const [isCompositionFiltered, setIsCompositionFiltered] = useState(false);
     const [generateCount, setGenerateCount] = useState(10);
 
-    if (!dropzones) {
-        setDropzones(Array.from({ length: 5 }, () => generateRandomDropzone({
-            world: generateWorld({
-                ca: generators[generator](caStateCount),
-            }),
-        })));
+    if (dropzones.length === 0) {
+        setDropzones(
+            Array.from({ length: generateCount },
+                () => generateRandomDropzone({
+                    world: generateWorld({
+                        ca: generators[generator](caStateCount),
+                    }),
+                })));
+    }
+
+    let filteredDropzones = dropzones;
+
+    const rockRange = [0.5, 1];
+    const energyRange = [0.00001, 0.1];
+    if (isCompositionFiltered) {
+        filteredDropzones = dropzones.filter((v) => {
+            const composition = getComposition(v.world.ca);
+
+            const [rock, _grass, energy] = composition
+                .map((p, i) => [p, i])
+                .sort(([a], [b]) => b - a)
+                .map(([_, i]) => i);
+            return composition[rock] >= rockRange[0]
+                && composition[rock] <= rockRange[1]
+                && composition[energy] >= energyRange[0]
+                && composition[energy] <= energyRange[1];
+        });
     }
 
     return <div {...props}>
-
         <label>
             Generator: <select
                 value={generator}
@@ -48,82 +68,63 @@ export function NewDropzones({
                     value={key}
                 >{key}</option>)}
             </select>
-        </label>
-        <br />
-        <label>
-            <input type="checkbox"
-                onChange={() => setIsFiltered(!isFiltered)} />
-            “filter stone &gt; 50% && energy &gt; 2%”
-        </label>
-        <br />
-        <label>
-            Generate count:
+        </label> =&gt; <label>
+            Count:
             <select
-                disabled={!isFiltered}
                 value={generateCount}
                 onChange={e => setGenerateCount(Number(e.target.value))}
             >
                 <option value="10">10</option>
+                <option value="30">30</option>
                 <option value="100">100</option>
-                <option value="1000">1000</option>
             </select>
-        </label>
-        <br />
-        <button
+        </label> =&gt; <button
             css={[{
                 margin: "0.9vmin 0",
             }]}
             onClick={() => {
-                if (isFiltered) {
-                    setDropzones(Array.from(
+                setDropzones(
+                    Array.from(
                         { length: generateCount },
                         () => generateRandomDropzone({
                             world: generateWorld({
                                 ca: generators[generator](caStateCount),
                             }),
-                        })).filter((v) => {
-                            const composition = getComposition(v.world.ca);
-                            const energy = 0.02;
-                            const stone = 0.5;
-                            return composition[0] > stone
-                                && composition[2] > energy;
-                        }));
-                } else {
-                    setDropzones(
-                        Array.from(
-                            { length: 5 },
-                            () => generateRandomDropzone({
-                                world: generateWorld({
-                                    ca: generators[generator](caStateCount),
-                                }),
-                            })));
-                }
-            }}> Reroll </button>
-        {dropzones
-            && <div css={[{
-                listStyle: "none",
-                display: "flex",
-                flexDirection: "row",
-                flexWrap: "wrap",
+                        })));
+            }}> (Re-)generate </button>
+        <br />
+        <label>
+            <input type="checkbox"
+                onChange={() =>
+                    setIsCompositionFiltered(!isCompositionFiltered)} />
+            filter rock in {JSON.stringify(rockRange)}
+            &nbsp;and energy in {JSON.stringify(energyRange)}
+        </label>
+        <br />
+        <div css={[{
+            listStyle: "none",
+            display: "flex",
+            flexDirection: "row",
+            flexWrap: "wrap",
+        }]}>
+            {filteredDropzones.map((dropzone, i) => <div key={i} css={[{
+                position: "relative",
             }]}>
-                {dropzones.map((dropzone, i) => <div key={i} css={[{
-                    position: "relative",
-                }]}>
-                    <DropzonePreview
-                        css={[{
-                            margin: "0.1vmin",
-                        }]}
-                        dropzone={dropzone} />
-                    <button
-                        css={[{
-                            position: "absolute",
-                            bottom: "1vmin",
-                            left: "50%",
-                            transform: "translateX(-50%)",
-                        }]}
-                        onClick={() => setDropzone(dropzone)}
-                    > Play!</button>
-                </div>)}
-            </div>}
+                <DropzonePreview
+                    css={[{
+                        margin: "0.1vmin",
+                    }]}
+                    dropzone={dropzone} />
+                <button
+                    css={[{
+                        position: "absolute",
+                        bottom: "1vmin",
+                        left: "50%",
+                        transform: "translateX(-50%)",
+                    }]}
+                    onClick={() => setDropzone(dropzone)}
+                > Play!</button>
+            </div>)}
+        </div>
     </div>;
 }
