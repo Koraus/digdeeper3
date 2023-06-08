43dee60f55370d55c029fe8554d1ef795b6d896e
Andriy Kashchynets <andriy.kashchynets@gmail.com>
2023-06-08 23:39:35 +0300

    drafted player move animation

--- src/app/mainSscene/PlayerView.tsx
+++ src/app/mainSscene/PlayerView.tsx
@@ -1,22 +1,11 @@
-import { ThreeElements } from "@react-three/fiber";
+import { ThreeElements, useThree } from "@react-three/fiber";
 import { useRecoilValue } from "recoil";
-import { TrekChain, sightAt, trekRecoil } from "../trekRecoil";
+import { sightAt, trekRecoil } from "../trekRecoil";
 import { Character } from "./Character";
+import { GroupSync } from "../../utils/GroupSync";
+import { MathUtils } from "three";
+import { easeSinInOut } from "d3-ease";
 
-
-const rotationForTrek = (trek: TrekChain) => {
-    if (!("prev" in trek)) { return 0; }
-
-    const curCop = sightAt(trek).playerPosition;
-    const prevCop = sightAt(trek.prev).playerPosition;
-
-    const [dx, dt] = [curCop[0] - prevCop[0], curCop[1] - prevCop[1]];
-
-    if (dx === 0 && dt === 0) { return 0; }
-    if (dx === 0) { return dt > 0 ? 1 : 3; }
-    if (dt === 0) { return dx > 0 ? 0 : 2; }
-    return 0;
-};
 
 export function PlayerView({
     children, ...props
@@ -24,16 +13,36 @@ export function PlayerView({
     const trek = useRecoilValue(trekRecoil);
     const sight = sightAt(trek);
     const pos = sight.playerPosition;
+    const prevPos = "prev" in trek ? sightAt(trek.prev).playerPosition : pos;
+    const [dx, dt] = [pos[0] - prevPos[0], pos[1] - prevPos[1]];
 
-    const rotation = rotationForTrek(trek);
+
+    const rotation = (() => {
+        if (dx === 0 && dt === 0) { return 0; }
+        if (dx === 0) { return dt > 0 ? 1 : 3; }
+        if (dt === 0) { return dx > 0 ? 0 : 2; }
+        return 0;
+    })();
+
+    const clock = useThree(({ clock }) => clock);
+    const tStart = clock.getElapsedTime();
 
     return <group
         {...props}
         position={[pos[1], 0, pos[0]]}
     >
-        <Character
-            rotation={[0, rotation * Math.PI / 2, 0]}
-        />
-        {children}
-    </group>;
+        <GroupSync
+            onFrame={(g, frame) => {
+                const dTimeSec = frame.clock.getElapsedTime() - tStart;
+                const t = MathUtils.clamp(dTimeSec * 8, 0, 1);
+                const t1 = easeSinInOut(t);
+                const t2 = t1 - 1;
+                g.position.z = dx * t2;
+                g.position.x = dt * t2;
+            }}
+        >
+            <Character rotation={[0, rotation * Math.PI / 2, 0]} />
+            {children}
+        </GroupSync>
+    </group >;
 }
