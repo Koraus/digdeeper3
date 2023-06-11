a0d0e97831d9fed18078d78f01998148d4fddb84
Andriy Kashchynets <andriy.kashchynets@gmail.com>
2023-06-11 11:51:25 +0300

    read initial analytics opt out value

--- src/app/initAnalytics.ts
+++ src/app/initAnalytics.ts
@@ -1,12 +1,15 @@
 import { init, setOptOut as _setOptOut, Types } from "@amplitude/analytics-browser";
 import { appVersion } from "./appVersion";
+import { optOutAnalyticsRecoil } from "./optOutAnalyticsRecoil";
 
 
 const amplitudeApiKey = import.meta.env["VITE_AMPLITUDE_API_KEY"];
 const analyticsEnabled = !!amplitudeApiKey && import.meta.env.PROD;
 init(amplitudeApiKey ?? "stub", undefined, {
     appVersion: appVersion,
-    optOut: !analyticsEnabled,
+    optOut:
+        JSON.parse(localStorage.getItem(optOutAnalyticsRecoil.key) ?? "false")
+        && !analyticsEnabled,
     attribution: {
         trackPageViews: true,
     },
