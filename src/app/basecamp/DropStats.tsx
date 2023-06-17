2ccf67c21e114ff11a47adca8d7da723f2768835
Koraus <dmytro.kashchynets@gmail.com>
2023-06-17 14:33:39 +0200

    Added bytecodeBase64 and version in the Drop Stats tab

--- src/app/basecamp/DropStats.tsx
+++ src/app/basecamp/DropStats.tsx
@@ -1,14 +1,33 @@
 import type { jsx } from "@emotion/react";
+import usePromise from "react-use-promise";
+import { fetchLastTreks } from "../fetchLastTreks";
+import { LoaderCircle } from "@emotion-icons/boxicons-regular/LoaderCircle";
 
 
 export function DropStats({
     ...props
 }: jsx.JSX.IntrinsicElements["div"]) {
 
+    const [treks] = usePromise(async () => {
+        const treks = await fetchLastTreks(100);
+        return treks;
+    }, []);
 
     return <div {...props}>
-        <div css={{ display: "flex", height: "100%" }}>
-            Drop Stats
+        <div css={{ overflow: "auto", height: "100%" }}>
+            {!treks && <LoaderCircle css={{ height: "8em", margin: "2em", }} />}
+            {treks &&
+                treks.map(
+                    (i, index) => {
+                        return <div key={index}
+                            css={{ borderBottom: "1px solid gray" }}>
+                            <p>
+                                {JSON.stringify(i.v)}</p>
+                            <p>
+                                {JSON.stringify(i.bytecodeBase64)}</p>
+                        </div>;
+                    },
+                )}
         </div>
-    </div>;
+    </div >;
 }
