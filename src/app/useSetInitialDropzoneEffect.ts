363bff2c5885674e221017f7f4269cfed1bf8d0e
Andriy Kashchynets <andriy.kashchynets@gmail.com>
2023-06-20 16:44:46 +0300

    fixed loading initial world offline

--- src/app/useSetInitialDropzoneEffect.ts
+++ src/app/useSetInitialDropzoneEffect.ts
@@ -4,6 +4,7 @@ import { generateRandomDropzone } from "../model/generate";
 import { devChoiceWorlds } from "./basecamp/DevChoiceWorlds";
 import { fetchDailyDateseed } from "./fetchDateseed";
 import { generateCuratedDailyDropzone } from "./NewGameInCuratedDailyDropzoneButton";
+import usePromise from "react-use-promise";
 
 
 const randomEl = <T>(arr: readonly T[]) =>
@@ -13,29 +14,32 @@ const prefetchedDailyDateseed = fetchDailyDateseed();
 
 // set the very first world once on start
 export function useSetInitialDropzoneEffect() {
-    const [done, setDone] = useState(false);
     const setDropzone = useSetDropzone();
-    useEffect(() => {
+    const [seed, , seedStatus] = usePromise(() => Promise.race([
         (async () => {
-            const seed = await Promise.race([
-                prefetchedDailyDateseed,
+            try {
+                return await prefetchedDailyDateseed;
+            } catch (ex) {
+                console.error(ex);
+                return undefined;
+            }
+        })(),
 
-                // give a chance to the curated daily world to load
-                // to avoid flickering
-                new Promise<undefined>(resolve => setTimeout(
-                    () => resolve(undefined),
-                    1000)),
-            ]);
-
-            setDropzone(
-                seed
-                    ? generateCuratedDailyDropzone(seed)
-                    : generateRandomDropzone({
-                        world: randomEl(devChoiceWorlds),
-                    }));
-
-            setDone(true);
-        })();
-    }, []);
-    return done;
+        // give a chance to the curated daily world to load
+        // to avoid flickering
+        // but fallback to random world if it takes too long
+        new Promise<undefined>(resolve => setTimeout(
+            () => resolve(undefined),
+            1000)),
+    ]), []);
+    useEffect(() => {
+        if (seedStatus !== "resolved") { return; }
+        setDropzone(
+            seed
+                ? generateCuratedDailyDropzone(seed)
+                : generateRandomDropzone({
+                    world: randomEl(devChoiceWorlds),
+                }));
+    }, [seedStatus]);
+    return seedStatus === "resolved";
 }
